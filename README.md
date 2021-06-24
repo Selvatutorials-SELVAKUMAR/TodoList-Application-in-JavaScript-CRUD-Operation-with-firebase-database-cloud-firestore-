86c942b9d8366d4fa81887de132f79539431750f
Selvatutorials-SELVAKUMAR <selvatutorials105@gmail.com>
2021-06-24 16:36:54 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>Display Current Date and Time using : <h1/>
+<ol>
+    <h2>HTML<h2/>
+    <h2>CSS<h2/>
+    <h2>MODERN JAVASCRIPT<h2/>
+</ol>
--- app.js
+++ app.js
@@ -1,137 +1,58 @@
-
- const db = firebase.firestore()
-
-const ul = document.querySelector('ul')
-const input = document.querySelector('input[type=text]')
-const add = document.querySelector('button[type=submit]')
-const popup = document.createElement('div')
-const popup_content = document.createElement('div')
-popup.classList.add('popup')
-popup_content.classList.add('popup-content')
-
-
-function todoop(li){
-    let close = document.createElement('span')
-    close.textContent = 'x'
-     close.classList.add('close')
-     li.appendChild(close)
-    
- 
-
-     // update button Element
-
-     
-       let update = document.createElement('button')
-        update.textContent = 'Update'
-         update.classList.add("update-btn")
-          li.appendChild(update)
-          update.addEventListener('click', e => {
-      e.stopPropagation()
-      let dataid = e.target.parentElement.id
-      let data = e.target.parentElement.data
-      const updatebtn = document.createElement('button')
-      const updatefield = document.createElement('input')
-      updatefield.type = 'text'
-       updatefield.value = data
-      updatefield.placeholder = "Update..."
-      updatebtn.classList.add('blue')
-      updatebtn.textContent = 'UPDATE'
-      popup_content.appendChild(updatefield)
-      popup_content.appendChild(updatebtn)
-      popup.appendChild(popup_content)
-      document.body.appendChild(popup)
-
-      updatebtn.addEventListener('click' , e => {
-          popup.classList.add('progress')
-          popup_content.classList.add('progress')
-          updatefield.classList.add('progress')
-          updatebtn.classList.add('progress')
-          if(updatefield.value === '') {
-              setTimeout(() => {
-                  popup.classList.remove('progress')
-                  popup_content.classList.remove('progress')
-                  updatefield.classList.remove('progress')
-                  updatebtn.classList.remove('progress')
-                  popup_content.removeChild(updatefield)
-                  popup_content.removeChild(updatebtn)
-                  popup.removeChild(popup_content)
-                  document.body.removeChild(popup)
-              },10);
-          } else {
-              db.collection('todos').doc(dataid).update({
-                  todo : updatefield.value
-              }).then(() => {
-                setTimeout(() => {
-                    popup.classList.remove('progress')
-                    popup_content.classList.remove('progress')
-                    updatefield.classList.remove('progress')
-                    updatebtn.classList.remove('progress')
-                    popup_content.removeChild(updatefield)
-                    popup_content.removeChild(updatebtn)
-                    popup.removeChild(popup_content)
-                    document.body.removeChild(popup)
-                },1150);
-              })
-      }
-      })
-  })
-     close.addEventListener('click' , e => {
-          e.stopPropagation()
-          let id = e.target.parentElement.id
-          db.collection('todos').doc(id).delete()
-      })
-}
-     
-
-function addTodo(doc){
-    // ListItem Element
-    
-      let li = document.createElement('li')
-       li.id = doc.id
-         li.textContent = doc.data().todo
-          li.data = doc.data().todo
-           doc.data().checked ? li.classList.add('checked') : li.classList.remove('checked')
-           ul.appendChild(li)
-    
-   // span Element
-    todoop(li)
-   
-}
-   
-// Add todos to database
-add.addEventListener('click',() => {
-    if(input.value === '') return
-    db.collection('todos').add({
-        id : new Date(),
-        todo : input.value,
-        checked : false
-    })
-    input.value = ''
-})
-// Real Time Listener
-db.collection('todos').orderBy('id').onSnapshot((snapshot) => {
-    let changes = snapshot.docChanges()
-    changes.forEach(change => {
-        if(change.type === 'added'){
-            addTodo(change.doc)
-        } else if(change.type === 'removed'){
-            let li = document.getElementById(change.doc.id)
-            ul.removeChild(li)
-        } else if(change.type === 'modified'){
-            setTimeout(() => {
-                let li = document.getElementById(change.doc.id)
-                li.textContent = change.doc.data().todo
-                li.data = change.doc.data().todo
-                change.doc.data().checked ? li.classList.add('checked') : li.classList.remove('checked')
-                todoop(li)
-            },1000);
+class CurrentDay_Time{
+    today
+    day
+    dayList
+    month
+    monthList
+    Date
+    year
+    hour
+    minute
+    second
+    am_pm
+    initializeValues(){
+        this.today = new Date()
+        this.day =  this.today.getDay()
+        this.dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+        this.monthList = ["Jan" , "Feb" , "Mar" , "Apr","May" , "June" , "July" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"]
+        this.month = this.monthList[this.today.getMonth()]
+        this.Date = this.today.getUTCDate()
+        this.year = this.today.getUTCFullYear()
+        this.hour =  this.today.getHours()
+        this.minute =  this.today.getMinutes()
+        this.second =  this.today.getSeconds()
+        this.am_pm = (this.today.getHours() >= 12)? "PM" : "AM"
+    }
+    displayCurrentDay(){
+        document.getElementById('day').textContent = `${this.dayList[this.day]} , ${this.month} ${this.Date} , ${this.year}`
+    }
+    takeTwelve(val){
+        if(val > 12){
+            return val-12
+        } else {
+            return val
         }
     }
-    )
-})
-ul.addEventListener('click' , e => {
-    e.target.classList.add('checked')
-    db.collection('todos').doc(e.target.id).update({
-        checked : true
-    })
-}) 
+    addZero(val){
+        if(val < 10){
+            return `0${val}`
+        } else {
+            return val
+        }
+    }
+    displayCurrentTime(){
+        document.getElementById('time').textContent = `Current Time = ${this.addZero(this.takeTwelve(this.hour))} : ${this.addZero(this.minute)} : ${this.addZero(this.second)} ${this.am_pm}`
+    }
+}
+
+const Object = new CurrentDay_Time()
+document.getElementsByTagName('button')[0].onclick = () => {
+    Object.initializeValues()
+    Object.displayCurrentDay()
+    Object.displayCurrentTime()
+    setInterval(() => {
+        Object.initializeValues()
+        Object.displayCurrentDay()
+        Object.displayCurrentTime()
+    },1000)
+}
